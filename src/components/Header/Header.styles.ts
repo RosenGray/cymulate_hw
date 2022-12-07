3d36636319e98b6fde6d8d134b8b59911238d52f
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2022-12-07 11:44:23 +0200

    adding another button

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -8,6 +8,7 @@ const StyledHeader = styled.header`
   padding:0 20px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
 export const Logo = styled.img``;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import Button from "../Button/Button";
 import StyledHeader, { Logo } from "./Header.styles"
 
 
 const Header = () => {
     return <StyledHeader>
         <Logo src="https://cymulate.com/wp-content/uploads/2021/08/cymulate-logo.png"/>
-    </StyledHeader>
+      <Button> Get Started</Button>
+   </StyledHeader>
 }
 
 export default Header ;
